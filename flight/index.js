3c7d0da75e51cad2e8991e4791d01a717c136361
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-15 12:22:31 +0600

    Creating modules with getters and setters

---
+++ flight/index.js
@@ -0,0 +1,21 @@
+var number, origin, destination;
+
+exports.setNumber = function (n) {
+    number = n;
+};
+
+exports.setOrigin = function (o) {
+    origin = o;
+};
+
+exports.setDestination = function (d) {
+    destination = d;
+};
+
+exports.getInfo = function () {
+    return {
+        number: number,
+        origin: origin,
+        destination: destination
+    };
+};
