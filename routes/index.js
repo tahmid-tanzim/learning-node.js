4e195b0a8c015ef655b42aa5feb85fe8bbaca256
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-15 17:36:37 +0600

    Handling GET requests returning JSON

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,30 @@
 /*
  * GET home page.
  */
+var flight = require('../flight');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var flight1 = flight.create({
+    number: 1,
+    origin: 'LAX',
+    destination: 'DCA',
+    departs: '9AM',
+    arrives: '4PM'
+});
+
+var flight2 = flight.create({
+    number: 2,
+    origin: 'LAX',
+    destination: 'PDX',
+    departs: '10AM',
+    arrives: '12PM'
+});
+
+exports.flight1 = function(req, res){
+    //res.render('index', { title: 'Express' });
+    res.json(flight1.getInformation());
+};
+
+exports.flight2 = function(req, res){
+    //res.render('index', { title: 'Express' });
+    res.json(flight2.getInformation());
 };
