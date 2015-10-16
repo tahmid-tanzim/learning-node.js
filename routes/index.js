bac3dab91b469fe20ba0e07ca886230120b7697a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-16 12:07:42 +0600

    Modifying data through PUT requests

--- routes/index.js
+++ routes/index.js
@@ -20,3 +20,20 @@ exports.flight = function (req, res) {
         res.json(flights[number].getInformation());
     }
 };
+
+exports.arrived = function (req, res) {
+    var number = req.param('number');
+
+    if (typeof flights[number] === 'undefined') {
+        res.status(404).json({
+            status: 'error',
+            message: 'Invalid flight number: ' + number
+        });
+    } else {
+        flights[number].triggerArrive();
+        res.json({
+            status: 'done',
+            info: flights[number].getInformation()
+        });
+    }
+};
