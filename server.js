3518d54300a70858bb23a5f86f28fe1e03c6658a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-19 14:40:55 +0600

    Constructing a custom REPL

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var flights = require('./data');
 var db = require('./db');
+var repl = require('repl');
 var app = require('./app')(flights, db);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+var prompt =  repl.start({prompt: 'flights> '});
+prompt.context.data = flights;
