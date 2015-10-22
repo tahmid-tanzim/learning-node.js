a6652e7551ed7b0a5410af3b504badc921051f39
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-22 09:34:20 +0600

    Streaming chunked data through readable streams

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(function (username, password, done) {
-    if(username === 'admin' && password === '12345') {
+    if (username === 'admin' && password === '12345') {
         return done(null, {username: 'admin'});
     } else {
         return done(null, false);
